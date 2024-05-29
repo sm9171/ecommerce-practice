ca752ef25508d270598e17427f5a0aba45b247c3
Sangmin <sm9171@nate.com>
2024-05-29 09:49:37 +0900

    Initial commit

---
+++ src/main/java/com/example/ecommerce_practice/EcommercePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercePracticeApplication.class, args);
+	}
+
+}
